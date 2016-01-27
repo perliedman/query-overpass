4afd9a11f38c1a639ad3442e932a6e8ea93fe9e2
Per Liedman <per@liedman.net>
2016-01-27 22:28:23 +0100

    Add basic tests

---
+++ test/specs.js
@@ -0,0 +1,34 @@
+var test = require('tape'),
+    queryOverpass = require('../');
+
+test('can execute basic query', function(t) {
+    queryOverpass('[out:json];node(57.7,11.9,57.8,12.0)[amenity=bar];out;', function(err, geojson) {
+        if (err) {
+            return t.fail(err);
+        }
+
+        t.ok(geojson, 'Should return data');
+        t.equal(geojson.type, 'FeatureCollection', 'Should return a FeatureCollection');
+        t.ok(geojson.features && geojson.features.length > 0, 'Should return at least one feature');
+
+        var f = geojson.features[0];
+        t.ok(f.properties && f.properties.tags, 'Properties are not flattened');
+        t.end();
+    });
+});
+
+test('can flatten properties', function(t) {
+    queryOverpass('[out:json];node(57.7,11.9,57.8,12.0)[amenity=restaurant];out;', function(err, geojson) {
+        if (err) {
+            return t.fail(err);
+        }
+
+        t.ok(geojson, 'Should return data');
+        t.equal(geojson.type, 'FeatureCollection', 'Should return a FeatureCollection');
+        t.ok(geojson.features && geojson.features.length > 0, 'Should return at least one feature');
+
+        var f = geojson.features[0];
+        t.ok(f.properties && !f.properties.tags && f.properties.name, 'Properties are not flattened');
+        t.end();
+    }, { flatProperties: true });
+});
